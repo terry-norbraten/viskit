a18ccdad834a1d7eee44d19fcd138492e3723ff5
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2004-04-26 20:22:35 +0000

    New packages structure - a number of classes will have to be regenerated
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@16 0de750ab-c0a9-dc11-9d74-0011435b89d6

---
+++ viskit/model/SchedulingEdge.java
@@ -0,0 +1,34 @@
+package viskit.model;
+
+import java.util.ArrayList;
+
+/**
+ * OPNAV N81 - NPS World Class Modeling (WCM) 2004 Projects
+ * MOVES Institute
+ * Naval Postgraduate School, Monterey CA
+ * www.nps.navy.mil
+ * By:   Mike Bailey
+ * Date: Mar 8, 2004
+ * Time: 9:04:09 AM
+ */
+
+public class SchedulingEdge extends Edge
+{
+  SchedulingEdge()       // package-limited
+  {
+    parameters = new ArrayList();
+  }
+  
+  Object copyShallow()
+  {
+    SchedulingEdge se = new SchedulingEdge();
+    se.opaqueViewObject = opaqueViewObject;
+    se.to = to;
+    se.from = from;
+    se.parameters = parameters;
+    se.delay = delay;
+    se.conditional = conditional;
+    se.conditionalsComment = conditionalsComment;
+    return se;
+  }
+}
