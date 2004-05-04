e302bdca8fca2a85a8fa55a0251b51970cebd25a
rickg <rickg@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2004-05-04 20:24:14 +0000

    support for indexed variable arrays
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@52 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/xsd/translator/SimkitXML2Java.java
+++ viskit/xsd/translator/SimkitXML2Java.java
@@ -35,18 +35,21 @@ public class SimkitXML2Java {
 
     /* convenience Strings for formatting */
 
-    String sp  = " ";
-    String sp4 = sp+sp+sp+sp;
-    String sp8 = sp4+sp4;
-    String ob  = "{";
-    String cb  = "}";
-    String sc  = ";";
-    String cm  = ",";
-    String lp  = "(";
-    String rp  = ")";
-    String eq  = "=";
-    String pd  = ".";
-    String qu  = "\"";
+    private final String sp  = " ";
+    private final String sp4 = sp+sp+sp+sp;
+    private final String sp8 = sp4+sp4;
+    private final String sp12= sp8+sp4;
+    private final String ob  = "{";
+    private final String cb  = "}";
+    private final String sc  = ";";
+    private final String cm  = ",";
+    private final String lp  = "(";
+    private final String rp  = ")";
+    private final String eq  = "=";
+    private final String pd  = ".";
+    private final String qu  = "\"";
+    private final String lb  = "[";
+    private final String rb  = "]";
 
     
     /** 
@@ -161,34 +164,113 @@ public class SimkitXML2Java {
 	PrintWriter pw = new PrintWriter(sw);
 	
 	pw.print(sp4 + "public void set" + capitalize(p.getName()) + lp);	
-	pw.print(p.getType() + sp + p.getShortName() + rp + sp + ob + sp);
-	pw.println(p.getName() + sp + eq + sp + p.getShortName() + sc + sp + cb);
+	pw.println(p.getType() + sp + shortinate(p.getName()) + rp + sp + ob);
+	pw.print(sp8 + p.getName() + sp + eq + sp);
+	
+	if ( isArray(p.getType()) ) {
+	    pw.print(lp + p.getType() + rp + sp + shortinate(p.getName()));
+	    pw.println(pd + "clone" + lp + rp + sc);
+	} else {
+	    pw.println(shortinate(p.getName()) + sc);
+	}
+	pw.println(sp4 + cb);
+	pw.println();	
+
+	// also provide indexed set/getters, may be multidimensional however not expected
+	// to actually be multidimensional
+	if ( isArray(p.getType()) ) {
+	    int d = dims(p.getType());
+
+	    pw.print(sp4 + "public void set" + capitalize(p.getName()) + lp + indx(d));
+	    pw.println(baseOf(p.getType()) + sp + shortinate(p.getName()) + rp + sp + ob);
+	    pw.println(sp8 + p.getName() + indxbr(d) + sp + eq + sp + shortinate(p.getName()) + sc);
+	    pw.println(sp4 + cb);
+	    pw.println();
+
+
+	    pw.print(sp4 + "public" + sp + baseOf(p.getType()) + sp + "get" );
+	    pw.print(capitalize(p.getName()) + lp + indxncm(d));
+	    pw.println(rp + sp + ob);
+	    pw.println(sp8 + "return" + sp + p.getName() + indxbr(d) + sc);
+	    pw.println(sp4 + cb);
+	    pw.println();
+	}
+	
 	pw.print(sp4 + "public " + p.getType() + sp + "get" + capitalize(p.getName()) );
-	pw.println(lp + rp + sp + ob + sp + "return" + sp + p.getName() + sc + sp + cb);
+	pw.println(lp + rp + sp + ob);
+	pw.println(sp8 + "return" + sp + p.getName() + sc);
+	pw.println(sp4 + cb);
 	pw.println();
     }
+
+ 
+    private int dims(String t) {
+	int d = 0;
+	int s = 0;
+
+	while ( (s = t.indexOf("[")) > 0 ) {
+	    d++;
+	    t = t.substring(s + 1);
+	}
+
+	return d;
+    }
+
+    private String indx(int dims) {
+	String inds = "";
+
+	for (int k = 0; k < dims; k++ ) {
+	    inds += "int" + sp + "i" + k + cm + sp;
+	}
+
+	return inds;
+    }
+
+    // trim off trailing comma space
+    private String indxncm(int dims) {
+	String ind = indx(dims);
+	return ind.substring(0, ind.length() - 2);
+    }
+	    
+    // creates [i0][i1]..[ik]
+    private String indxbr(int dims) {
+	String inds = "";
+
+	for (int k = 0; k < dims; k++ ) {
+	    inds += lb + "i" + k + rb;
+	}
+
+	return inds;
+    }
+
 
     void buildStateVariableAccessor(StateVariable s, StringWriter sw) {
 
         PrintWriter pw = new PrintWriter(sw);
 	String clStr = "";
-	Class aClass = null;
 
 	// check for cloneable 
 
-	try {
-	    aClass = Thread.currentThread().getContextClassLoader().loadClass(s.getType());
-	} catch ( ClassNotFoundException cnfe ) {
-	}
-	
-	if (aClass != null) {
-	    if ( java.lang.Cloneable.class.isAssignableFrom(aClass) ) {
-	        clStr = ".clone()";
-	    } 
+	if (isCloneable(s.getType())) {
+	    clStr = ".clone()";
 	}
 
         pw.print(sp4 + "public " + s.getType() + sp + "get" + capitalize(s.getName()) );
-        pw.println(lp + rp + sp + ob + sp + "return" + sp + s.getName() + clStr + sc + sp + cb);
+        pw.println(lp + rp + sp + ob);
+	pw.println(sp8 + "return" + sp + s.getName() + clStr + sc);
+	pw.println(sp4 + cb);
+	pw.println();
+
+	if ( isArray(s.getType()) ) {
+	    int d = dims(s.getType());
+	    pw.print(sp4 + "public" + sp + baseOf(s.getType()) + sp + "get" );
+	    pw.print(capitalize(s.getName()) + lp + indxncm(d));
+	    pw.println(rp + sp + ob);
+	    pw.println(sp8 + "return" + sp + s.getName() + indxbr(d) + sc);
+	    pw.println(sp4 + cb);
+	    pw.println();
+	}
+
         pw.println();
     }
 
@@ -217,6 +299,10 @@ public class SimkitXML2Java {
 	ListIterator li;
 	List sched = run.getScheduleOrCancel();
 	ListIterator schi = sched.listIterator();
+	
+	// w/out a tag, can only assume arrays in parameters
+	// are all the same size...
+	String firstArray=null;
 
 	pw.println();
 	pw.println(sp4 + "/** Creates a new instance of " + this.root.getName() + " */");
@@ -230,7 +316,7 @@ public class SimkitXML2Java {
 
 	    Parameter pt = (Parameter) li.next();
 		
-	    pw.print(pt.getType() + sp + pt.getShortName());
+	    pw.print(pt.getType() + sp + shortinate(pt.getName()));
 	
 	    if ( pList.size() > 1 ) {
  	        if ( pList.indexOf(pt) <  pList.size() - 1 ) {
@@ -249,13 +335,40 @@ public class SimkitXML2Java {
 	while ( li.hasNext() ) {
 	    Parameter pt = (Parameter) li.next();
 	    pw.println(sp8 + "set" + capitalize(pt.getName()) + 
-		lp + pt.getShortName() + rp + sc);
+		lp + shortinate(pt.getName()) + rp + sc);
+	    if( firstArray == null ) {
+		firstArray = (isArray(pt.getType()))?pt.getName():null;
+	    }
 	}
+
+	// create new arrays, if any
+	// note: have to assume that the length of parameter arrays
+	// is consistent 
+
+	li = this.root.getStateVariable().listIterator();
+
+	while ( li.hasNext() ) {
+	    StateVariable st = (StateVariable) li.next();
+	    if (isArray(st.getType())) {
+		pw.print(sp8 + st.getName() + sp + eq + sp + "new" + sp + baseOf(st.getType()));
+		pw.println(lb + shortinate(firstArray) + pd + "length" + rb + sc);
+	    }
+	}	
+	
 
 	pw.println(sp4 + cb);
 	pw.println();
 	pw.println(sp4 + "/** Set initial values of all state variables */");
 	pw.println(sp4 + "public void reset() {");
+
+	li = run.getLocalVariable().listIterator();
+
+	while ( li.hasNext() ) {
+	    LocalVariable local = (LocalVariable) li.next();
+	    pw.println(sp8 + local.getType() + sp + local.getName() + sc);
+	}
+	
+	pw.println();
 	pw.println(sp8 + "super.reset()" + sc);
 	pw.println();
 	pw.println(sp8 + "/** StateTransitions for the Run Event */");
@@ -268,11 +381,26 @@ public class SimkitXML2Java {
 	    StateVariable sv = (StateVariable) st.getState();
 	    AssignmentType asg = st.getAssignment();
 	    OperationType ops = st.getOperation();
-	    pw.print(sp8 + sv.getName());
-	    if ( asg == null ) { 
-		pw.println( pd + ops.getMethod() + sc );
+	    boolean isar = isArray(sv.getType());
+	    String spn = isar ? sp12:sp8;
+	    String in = indexFrom(st);
+	
+	    if ( isar ) {
+		pw.print(sp8 + "for (" + in + " = 0; " + in + " < " + sv.getName() + pd + "length");
+		pw.println(sc + sp + in + "++" + rp + sp + ob);
+	        pw.print(spn + sv.getName() + lb + in + rb);
 	    } else {
-		pw.println( sp + eq + sp + asg.getValue() + sc );
+		pw.print(spn + sv.getName());
+	    }
+
+	    if ( asg == null ) { 
+		pw.println(pd + ops.getMethod() + sc);
+	    } else {
+		pw.println(sp + eq + sp + asg.getValue() + sc);
+	    }
+
+	    if ( isar ) {
+		pw.println(sp8 + cb);
 	    }
 	}
 
@@ -282,15 +410,11 @@ public class SimkitXML2Java {
 	pw.println(sp4 + "public void doRun() {");
 	
 	li = run.getStateTransition().listIterator();
-	
-	// because the run event gets split in twain this is 
-	// similar as above
 
 	while( li.hasNext() ) {
 	    StateTransition st = (StateTransition) li.next();
 	    StateVariable sv = (StateVariable) st.getState();
-	    AssignmentType asg = st.getAssignment();
-	    OperationType ops = st.getOperation();
+	    // no fireIndexedPropertyChanged in doRun?
 	    pw.print(sp8 + "firePropertyChange(" + qu + sv.getName() + qu); 
 	    pw.println(cm + sv.getName() + rp + sc);
 	}
@@ -335,7 +459,7 @@ public class SimkitXML2Java {
 
 	// local variable decls
 	while ( lci.hasNext() ) {
-	    LocalVariable local = (LocalVariable) lci.next();
+	    LocalVariableType local = (LocalVariableType) lci.next();
 	    pw.print(sp8 + local.getType() + sp + local.getName() + sp + eq);
 	    pw.print(sp + lp + local.getType() + rp);
 	    pw.println(sp + local.getValue() + sc);
@@ -348,20 +472,29 @@ public class SimkitXML2Java {
 	    StateVariable sv = (StateVariable) st.getState();
 	    AssignmentType asg = st.getAssignment();
 	    OperationType ops = st.getOperation(); 
-	
+
 	    pw.print(sp8 + sv.getName());
-	    if (asg == null) {
+
+	    pw.print(lb + indexFrom(st) + rb);
+
+	    if (ops != null) {
 		pw.println(pd + ops.getMethod() + sc);
-	    } else {
+	    } else if (asg != null) {
 		pw.println(sp + eq + sp + asg.getValue() + sc);
 	    }
-	    pw.print(sp8 + "firePropertyChange" + lp + qu + sv.getName() + qu + cm + sp);
-	    pw.println(sv.getName() + rp + sc);
+	
+	    if ( isArray(sv.getType()) ) {
+		pw.print(sp8 + "fireIndexedPropertyChange" + lp + indexFrom(st)); 
+		pw.println(cm + sp + qu + sv.getName() + qu + cm + sp + sv.getName() + rp + sc);
+	    } else {
+	        pw.print(sp8 + "firePropertyChange" + lp + qu + sv.getName() + qu + cm + sp);
+	        pw.println(sv.getName() + rp + sc);
+	    }
 	    pw.println();
 
 	}
 
-	// wailDelay/interrupt
+	// waitDelay/interrupt
 	while ( schi.hasNext() ) {
 	    Object o = schi.next();
 	    if ( o instanceof ScheduleType ) {
@@ -509,6 +642,63 @@ public class SimkitXML2Java {
         return s.substring(0,1).toUpperCase() + s.substring(1);
     }
 
+    private String indexFrom(StateTransitionType st) {
+	String index = "i"; // fallback guess
+
+	if (st.getIndex() instanceof LocalVariableType)
+	    index = ((LocalVariableType)st.getIndex()).getName();
+	else if (st.getIndex() instanceof ArgumentType)
+	    index = ((Argument)st.getIndex()).getName();
+	else if (st.getIndex() instanceof ParameterType)
+	    index = ((ParameterType)st.getIndex()).getName();
+
+	return index;
+    }
+
+    private String shortinate( String s ) {
+
+	String result = s.substring(0,1);
+	char[] ca = s.toCharArray();
+	char[] tmp = new char[1];
+	
+	for ( int i = 1; i < ca.length; i++ ) {
+	    if ( Character.isUpperCase(ca[i]) ) {
+		tmp[0] = ca[i];
+		result += new String(tmp);
+	    }
+	}
+
+	return result.toLowerCase();
+    }
+
+    private String baseOf( String s ) {
+	return s.substring(0,s.indexOf("["));
+    }
+
+    private String indexIn( String s ) {
+	return s.substring(s.indexOf("[")+1, s.indexOf("]")-1);
+    }
+
+    private boolean isCloneable( String c ) {
+
+	Class aClass = null;
+
+	try {
+	    aClass = Thread.currentThread().getContextClassLoader().loadClass(c);
+	} catch ( ClassNotFoundException cnfe ) {;}
+	
+	if (aClass != null) {
+	    return java.lang.Cloneable.class.isAssignableFrom(aClass);
+	} else if (isArray(c)) {
+	    return true;
+	} else
+	    return false;
+    }
+
+    private boolean isArray( String c ) {
+	if ( c.endsWith(rb) ) return true;
+	else return false;
+    }
 
     void error(String desc) {
 
