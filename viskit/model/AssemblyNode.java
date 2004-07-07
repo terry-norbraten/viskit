b0620dda81aa6f0baad6c8c45013fc31d0f37b4f
jmbailey <jmbailey@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2004-07-07 21:55:22 +0000

    "Don't try saving and restoring yet...this is for Rick to test"
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@137 0de750ab-c0a9-dc11-9d74-0011435b89d6

---
+++ viskit/model/AssemblyNode.java
@@ -0,0 +1,99 @@
+package viskit.model;
+
+import java.util.Vector;
+import java.util.ArrayList;
+import java.awt.*;
+
+/**
+ * OPNAV N81 - NPS World Class Modeling (WCM)  2004 Projects
+ * MOVES Institute
+ * Naval Postgraduate School, Monterey, CA
+ * www.nps.edu
+ * By:   Mike Bailey
+ * Date: Jul 1, 2004
+ * Time: 9:47:55 AM
+ */
+
+abstract public class AssemblyNode extends ViskitElement
+{
+  private String name;
+  private String type;
+
+  private Vector    connections = new Vector();
+  private ArrayList comments = new ArrayList();
+  private Point     position = new Point(0,0);
+  private VInstantiator instantiator;
+
+  AssemblyNode(String name, String type)      // package access on constructor
+  {
+    this.name = name;
+    this.type = type;
+    instantiator = new VInstantiator.FreeF(type,"");
+  }
+  public String toString()
+  {
+    return name;
+  }
+  public String getName()
+  {
+    return name;
+  }
+  public void setName(String s)
+  {
+    this.name = name;
+  }
+  public String getType()
+  {
+    return type;
+  }
+  public void setType(String typ)
+  {
+   //todo if(this.opaqueModelObject != null)
+   //   ((Event)opaqueModelObject).setName(s);
+
+    this.type = typ;
+
+  }
+
+  public ArrayList getComments()
+  {
+    return comments;
+  }
+
+  public void setComments(ArrayList comments)
+  {
+    this.comments = comments;
+  }
+
+  public Vector getConnections()
+  {
+    return connections;
+  }
+
+  public void setConnections(Vector connections)
+  {
+    this.connections = connections;
+  }
+
+  public Point getPosition()
+  {
+    return position;
+  }
+
+  public void setPosition(Point position)
+  {
+    this.position = position;
+  }
+
+  public VInstantiator getInstantiator()
+  {
+    return instantiator;
+  }
+
+  public void setInstantiator(VInstantiator instantiator)
+  {
+    this.instantiator = instantiator;
+  }
+
+
+}
