203b5e88b4aea2cccefa3a51fb5b94e5a02f7caa
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2004-08-30 20:28:03 +0000

    first cut at shell script for Unix and Un*x systems...
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@216 0de750ab-c0a9-dc11-9d74-0011435b89d6

---
+++ run.sh
@@ -0,0 +1,5 @@
+java -version
+#
+# This assumes that '$ ant jar' or '$ ant compilt' has already been executed.
+#
+java -classpath lib/actions.jar:lib/bsh-2.0b1.jar:lib/jdom.jar:lib/jgraph.jar:lib/simkit.jar:lib/tools.jar:lib/trang.jar:lib/trangtask.jar:lib/jwsdp-1.3/dom.jar:lib/jwsdp-1.3/jax-qname.jar:lib/jwsdp-1.3/jaxb-api.jar:lib/jwsdp-1.3/jaxb-impl.jar:lib/jwsdp-1.3/jaxb-libs.jar:lib/jwsdp-1.3/jaxb-xjc.jar:lib/jwsdp-1.3/jaxp-api.jar:lib/jwsdp-1.3/namespace.jar:lib/jwsdp-1.3/relaxngDatatype.jar:lib/jwsdp-1.3/sax.jar:lib/jwsdp-1.3/xalan.jar:lib/jwsdp-1.3/xercesImpl.jar:lib/jwsdp-1.3/xsdlib.jar:lib/jwsdp-1.3/xsltc.jar:build/classes viskit.Splash2
