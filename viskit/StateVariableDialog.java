3af7c495894051073043624a130b28189a225551
jmbailey <jmbailey@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2004-10-29 22:14:07 +0000

    Correct array support, fix focus handling and default button assignment.
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@298 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/StateVariableDialog.java
+++ viskit/StateVariableDialog.java
@@ -3,21 +3,21 @@ package viskit;
 import viskit.model.vStateVariable;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
- * A dialog class that lets the user add a new state variable to the document.
- * After the user clicks "OK", "Cancel", or closes the dialog via the
- * close box, the caller retrieves the "buttonChosen" variable from
- * the object to determine the choice. If the user clicked "OK", the
- * caller can retrieve various choices from the object.
- *
- * @author DMcG
+ * OPNAV N81 - NPS World Class Modeling (WCM) 2004 Projects
+ * MOVES Institute
+ * Naval Postgraduate School, Monterey CA
+ * www.nps.navy.mil
+ * By:   Mike Bailey
  */
- 
+
 public class StateVariableDialog extends ViskitSmallDialog
 {
   private JTextField stateVarNameField;    // Text field that holds the parameter name
@@ -27,11 +27,14 @@ public class StateVariableDialog extends ViskitSmallDialog
   private JLabel     arrSizeLab;
 
   private vStateVariable stVar;
-  private Component locationComp;
-  private JButton okButt, canButt;
-  static private int count = 0;
+  private Component      locationComp;
+  private JButton        okButt, canButt;
+  static private int     count = 0;
 
   public static String newName, newType, newComment;
+
+  private myFocusListener focList;
+  private Component       myTyperComponent;       // i.e., the editor of the type JComboBox
 
   public static boolean showDialog(JFrame f, Component comp, vStateVariable var)
   {
@@ -41,6 +44,8 @@ public class StateVariableDialog extends ViskitSmallDialog
   protected StateVariableDialog(JFrame parent, Component comp, Object param)
   {
     super(parent, "State Variable Declaration", true);
+    focList = new myFocusListener();
+
     this.stVar = (vStateVariable)param;
     this.locationComp = comp;
 
@@ -63,10 +68,20 @@ public class StateVariableDialog extends ViskitSmallDialog
 
         int w = maxWidth(new JComponent[]{nameLab,initLab,typeLab,commLab});
 
-        stateVarNameField = new JTextField(15);   setMaxHeight(stateVarNameField);
-        commentField       = new JTextField(25);   setMaxHeight(commentField);
-        stateVarTypeCombo = VGlobals.instance().getTypeCB(); setMaxHeight(stateVarTypeCombo);
+        stateVarNameField = new JTextField(15);
+        stateVarNameField.addFocusListener(focList);
+        setMaxHeight(stateVarNameField);
+
+        commentField = new JTextField(25);
+        commentField.addFocusListener(focList);
+        setMaxHeight(commentField);
+
+        stateVarTypeCombo = VGlobals.instance().getTypeCB();
+        stateVarTypeCombo.getEditor().getEditorComponent().addFocusListener(focList);
+        setMaxHeight(stateVarTypeCombo);
+
         arraySizeField    = new JTextField(15);
+        arraySizeField.addFocusListener(focList);
 
         fieldsPanel.add(new OneLinePanel(nameLab,w,stateVarNameField));
         fieldsPanel.add(new OneLinePanel(typeLab,w,stateVarTypeCombo));
@@ -90,31 +105,28 @@ public class StateVariableDialog extends ViskitSmallDialog
 
     modified        = (param==null?true:false);     // if it's a new stVar, they can always accept defaults with no typing
     okButt.setEnabled((param==null?true:false));
+    if(okButt.isEnabled())
+      getRootPane().setDefaultButton(okButt);
+    else
+      getRootPane().setDefaultButton(canButt);
+
     arrSizeLab.setEnabled(false);
-    arraySizeField.setEditable(false);
-    getRootPane().setDefaultButton(canButt);
+    arraySizeField.setEnabled(false);
+    arraySizeField.setEditable(false);     // grays background if false
 
     pack();     // do this prior to next
     this.setLocationRelativeTo(locationComp);
 
     // attach listeners
     canButt.addActionListener(new cancelButtonListener());
-    okButt .addActionListener(new applyButtonListener());
+    okButt .addActionListener(new StateVarApplyButtonListener());//applyButtonListener());
 
     enableApplyButtonListener lis = new enableApplyButtonListener(okButt);
-    stateVarNameField.addCaretListener(lis);
-    commentField.      addCaretListener(lis);
+    stateVarNameField.getDocument().addDocumentListener(lis);//addCaretListener(lis);
+    commentField.     getDocument().addDocumentListener(lis);// addCaretListener(lis);
     stateVarTypeCombo.addActionListener(lis);
-    // also, check for array
-    stateVarTypeCombo.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        boolean isArray = ((String)stateVarTypeCombo.getSelectedItem()).indexOf('[') != -1;
-        arraySizeField.setEditable(isArray);
-        arrSizeLab.setEnabled(isArray);
-      }
-    });
+
+    myTyperComponent = stateVarTypeCombo.getEditor().getEditorComponent();
 
     setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
     addWindowListener(new WindowClosingListener(this,okButt,canButt));
@@ -129,27 +141,51 @@ public class StateVariableDialog extends ViskitSmallDialog
 
     modified        = (p==null?true:false);
     okButt.setEnabled((p==null?true:false));
-
-    getRootPane().setDefaultButton(canButt);
+    if(p == null)
+      getRootPane().setDefaultButton(okButt);
+    else
+      getRootPane().setDefaultButton(canButt);
 
     this.setLocationRelativeTo(c);
   }
 
+  private String stripArraySize(String typ)
+  {
+    Pattern p = Pattern.compile("\\[.*?\\]");
+    Matcher m = p.matcher(typ);
+    return m.replaceAll("[]");
+  }
+
+  private String getArraySize(String typ)
+  {
+    Pattern p = Pattern.compile("\\[.*?\\]");
+    Matcher m = p.matcher(typ);
+    if(m.find()) {
+      String f = m.group();
+      return f.substring(1,f.length()-1);
+    }
+    else
+      return "";
+
+  }
   private void fillWidgets()
   {
     if(stVar != null) {
       stateVarNameField.setText(stVar.getName());
-      stateVarTypeCombo.setSelectedItem(stVar.getType());
-      //setType(stVar.getType(),stateVarTypeCombo);
+      String ty = stVar.getType();
+      stateVarTypeCombo.setSelectedItem(stripArraySize(ty));
+      arraySizeField.setText(getArraySize(ty));
       commentField.setText(stVar.getComment());
       boolean isArray = stVar.getType().indexOf('[') != -1;
-      arraySizeField.setEditable(isArray);
+      arraySizeField.setEditable(isArray);   // grays background if false
+      arraySizeField.setEnabled(isArray);
       arrSizeLab.setEnabled(isArray);
     }
     else {
       stateVarNameField.setText("state_"+count++);
       commentField.setText("");
-      arraySizeField.setEditable(false);
+      arraySizeField.setEditable(false); // grays out background
+      arraySizeField.setEnabled(false);
       arrSizeLab.setEnabled(false);
     }
     stateVarNameField.requestFocus();
@@ -162,6 +198,9 @@ public class StateVariableDialog extends ViskitSmallDialog
     // make sure there are no spaces
     String ty = (String)stateVarTypeCombo.getSelectedItem();
     ty = VGlobals.instance().typeChosen(ty);
+    if(isArray(ty)) {
+      ty=ty.substring(0,ty.indexOf('[')+1) + arraySizeField.getText().trim() + "]";
+    }
     String nm = stateVarNameField.getText();
     nm = nm.replaceAll("\\s","");
 
@@ -174,6 +213,130 @@ public class StateVariableDialog extends ViskitSmallDialog
       newName = nm;
       newType = ty;
       newComment = commentField.getText().trim();
+    }
+  }
+
+  private boolean isArray(String s)
+  {
+    return s.indexOf('[') == -1 ? false:true;
+  }
+
+  private boolean isGoodArray(String s)
+  {
+    s = s.trim();
+    int brkIdx = s.indexOf('[');
+    if(brkIdx == -1)
+      return true;
+
+    Pattern p = Pattern.compile(".+\\[\\s*\\](^\\[\\]){0}$");     // blah[whitsp]<eol>
+    Matcher m = p.matcher(s);
+    return m.matches();
+  }
+  // Little classes to move the focus around
+  private sizeFieldFocus       sizeFocus = new sizeFieldFocus();
+  private commentFieldFocus commentFocus = new commentFieldFocus();
+
+  class sizeFieldFocus implements Runnable
+  {
+    public void run()
+    {
+      arraySizeField.requestFocus();
+    }
+  }
+  class commentFieldFocus implements Runnable
+  {
+    public void run()
+    {
+      commentField.requestFocus();
+    }
+  }
+
+  class myFocusListener extends FocusAdapter
+  {
+    public void focusGained(FocusEvent e)
+    {
+      handleSelect(e.getComponent());
+
+      if (e.getOppositeComponent() == myTyperComponent)
+        handleArrayFieldEnable();
+    }
+
+    /**
+     *  Enable the array size field if the type is an array, and set the focus to the right guy.
+     */
+    private void handleArrayFieldEnable()
+    {
+      String s = (String) stateVarTypeCombo.getEditor().getItem();
+      boolean isAr = isArray(s);
+      arraySizeField.setEditable(isAr); // grays background if false
+      arraySizeField.setEnabled(isAr);
+      arrSizeLab.setEnabled(isAr);
+
+      // Do this this way to shake out all the pending focus events before twiddling focus.
+      if (isAr)
+        SwingUtilities.invokeLater(sizeFocus);
+      else
+        SwingUtilities.invokeLater(commentFocus);
+    }
+
+    /**
+     * select the text in whatever comes in
+     * @param c
+     */
+    private void handleSelect(Component c)
+    {
+      if (c instanceof ComboBoxEditor)
+        c = ((ComboBoxEditor) c).getEditorComponent();
+
+      if (c instanceof JTextComponent)
+        ((JTextComponent) c).selectAll();
+      else if (c instanceof ComboBoxEditor)
+        ((ComboBoxEditor) c).selectAll();
+      else if (c instanceof JButton)
+        ;
+    }
+  }
+
+  class StateVarApplyButtonListener implements ActionListener
+  {
+    public void actionPerformed(ActionEvent event)
+    {
+      if (modified) {
+        String typ = ((String) stateVarTypeCombo.getSelectedItem()).trim();
+        String nam = stateVarNameField.getText().trim();
+        String arsz = arraySizeField.getText().trim();
+
+        if (nam.length() <= 0 ||
+            typ.length() <= 0 ||
+            (isArray(typ) && arsz.length() <= 0)) {
+          JOptionPane.showMessageDialog(StateVariableDialog.this, "Name, type and (if array) array size must be entered.",
+              "Data entry error", JOptionPane.ERROR_MESSAGE);
+          return;
+        }
+        else if (isArray(typ) && !isGoodArray(typ)) {
+          JOptionPane.showMessageDialog(StateVariableDialog.this, "Use a single trailing pair of empty square brackets\nto signify a one-dimensional array.",
+              "Data entry error", JOptionPane.ERROR_MESSAGE);
+          return;
+        }
+
+        // Do a beanshell test for array declaration
+        if (!VGlobals.instance().isPrimitive(typ) && isArray(typ)) {         // isPrimitive returns false for arrays
+
+          String s = typ + " " + nam + " = new " + typ;
+          s = s.substring(0, s.lastIndexOf('[') + 1) + arsz + "]";          // stick in size
+
+          String result = VGlobals.instance().parseCode(null, s);
+          if (result != null) {
+            JOptionPane.showMessageDialog(StateVariableDialog.this, result,
+                "Data entry error", JOptionPane.ERROR_MESSAGE);
+            return;
+          }
+        }
+        // ok, we passed
+        unloadWidgets();
+
+      }
+      setVisible(false);
     }
   }
 }
