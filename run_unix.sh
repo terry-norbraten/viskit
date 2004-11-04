94cf2835ff1011ddd47f910dd5052ca3bfdebfbf
rickg <rickg@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2004-11-04 19:10:22 +0000

    simple run scripts for cygwin and unix
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@300 0de750ab-c0a9-dc11-9d74-0011435b89d6

---
+++ run_unix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Build a classpath by looking for jars in the right places. Doesn't pack the  
+# most minimal set needed for runtime, but is quick.
+
+
+export BASEDIR=${HOME}/CVS/Viskit
+export LIBDIR=${BASEDIR}/lib
+export LIBJWSDPDIR=${LIBDIR}/jwsdp-1.4
+export LIBEXTDIR=${LIBDIR}/ext
+export LIBBUILDDIR=${BASEDIR}/build/lib
+export CLASSPATH=
+
+
+for i in $LIBJWSDPDIR/*.jar $LIBDIR/*.jar $LIBEXTDIR/*.jar $LIBBUILDDIR/*.jar
+do
+	CLASSPATH=$i:$CLASSPATH
+done
+
+
+java -classpath $CLASSPATH viskit.Splash2
+
