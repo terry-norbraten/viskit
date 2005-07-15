0688c0c6dc209bcf7061cdb8e6cdc7ebee7f2b62
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2005-07-15 23:29:00 +0000

    Update version number to 0.1.7
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@404 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/Help.java
+++ viskit/Help.java
@@ -14,7 +14,7 @@ import javax.help.HelpSetException;
  */
 public class Help {
     
-    public static final String VERSION = "0.1.6";
+    public static final String VERSION = "0.1.7";
     
     private Component parent;
     private Icon icon;
