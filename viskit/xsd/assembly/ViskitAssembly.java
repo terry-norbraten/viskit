febc756b736ca73b830b0568a5eada56c373319a
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2005-09-29 15:33:00 +0000

    Removed generics
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@485 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/xsd/assembly/ViskitAssembly.java
+++ viskit/xsd/assembly/ViskitAssembly.java
@@ -6,7 +6,7 @@
  * BasicAssembly doesn't provide Viskit users with ability to 
  * reference a known SimEntity within a constructor. This class 
  * provides hooks into the BasicAssembly and enables named references
- * to instances within the design tool. Note accidental use of generics.
+ * to instances within the design tool. 
  */
 
 package viskit.xsd.assembly;
@@ -16,9 +16,10 @@ import simkit.stat.*;
 import java.util.*;
 import java.beans.PropertyChangeListener;
 import java.lang.reflect.Constructor;
+import viskit.xsd.bindings.assembly.PropertyChangeListenerConnection;
 
 /**
- *
+ * @version $Id$
  * @author Rick Goldberg
  */
 public class ViskitAssembly extends BasicAssembly { 
@@ -45,7 +46,7 @@ public class ViskitAssembly extends BasicAssembly {
     }
     
     public void hookupReplicationListeners() {
-        String[] listeners = (new ArrayList<String>(replicationStatsListenerConnections.keySet())).toArray(new String[0]);
+        String[] listeners = (String[]) replicationStatsListenerConnections.keySet().toArray(new String[0]);
         for ( int i = 0; i < listeners.length; i++ ) {
             PropertyConnector pc = (PropertyConnector)replicationStatsListenerConnections.get(listeners[i]);
             connectReplicationStat(listeners[i], pc.source);
@@ -54,7 +55,7 @@ public class ViskitAssembly extends BasicAssembly {
     }
     
     public void hookupSimEventListeners() {
-        String[] listeners = (new ArrayList<String>(simEventListenerConnections.keySet())).toArray(new String[0]);
+        String[] listeners = (String[]) simEventListenerConnections.keySet().toArray(new String[0]);
         for ( int i = 0; i < listeners.length; i++ ) {
             String source = (String) simEventListenerConnections.get(listeners[i]);
             connectSimEventListener(listeners[i], source);
@@ -63,7 +64,7 @@ public class ViskitAssembly extends BasicAssembly {
     }
     
     public void hookupPropertyChangeListeners() {
-        String[] listeners = (new ArrayList<String>(propertyChangeListenerConnections.keySet())).toArray(new String[0]);
+        String[] listeners = (String[]) propertyChangeListenerConnections.keySet().toArray(new String[0]);
         for ( int i = 0; i < listeners.length; i++ ) {
             PropertyConnector pc = (PropertyConnector)propertyChangeListenerConnections.get(listeners[i]);
             connectPropertyChangeListener(listeners[i], pc.source);
@@ -71,7 +72,7 @@ public class ViskitAssembly extends BasicAssembly {
     }
     
     public void hookupDesignPointListeners() {
-        String[] listeners = (new ArrayList<String>(designPointStatsListenerConnections.keySet())).toArray(new String[0]);
+        String[] listeners = (String[]) designPointStatsListenerConnections.keySet().toArray(new String[0]);
         for ( int i = 0; i < listeners.length; i++ ) {
             PropertyConnector pc = (PropertyConnector)designPointStatsListenerConnections.get(listeners[i]);
             connectDesignPointStat(listeners[i], pc.source);
@@ -100,12 +101,12 @@ public class ViskitAssembly extends BasicAssembly {
     /*  note not using template version of ArrayList... */
     public void createSimEntities() {
         simEntity = 
-                (new ArrayList<SimEntity>(entities.values())).toArray(new SimEntity[0]);
+                (SimEntity[]) entities.values().toArray(new SimEntity[0]);
     }
     
     public void createDesignPointStats() {
         designPointStats = 
-                (new ArrayList<SampleStatistics>(designPointStatistics.values())).toArray(new SampleStatistics[0]);
+                (SampleStatistics[]) designPointStatistics.values().toArray(new SampleStatistics[0]);
         // set up some defaults from the super method
         if ( designPointStats.length == 0 ) {
             super.createDesignPointStats();
@@ -114,12 +115,12 @@ public class ViskitAssembly extends BasicAssembly {
     
     public void createReplicationStats() {
         replicationStats = 
-                (new ArrayList<SampleStatistics>(replicationStatistics.values())).toArray(new SampleStatistics[0]);
+                (SampleStatistics[]) replicationStatistics.values().toArray(new SampleStatistics[0]);
     }
     
     public void createPropertyChangeListeners() {
         propertyChangeListener = 
-                (new ArrayList<PropertyChangeListener>(propertyChangeListeners.values())).toArray(new PropertyChangeListener[0]);
+                (PropertyChangeListener[]) propertyChangeListeners.values().toArray(new PropertyChangeListener[0]);
     }
     
     public void addSimEntity(String name, SimEntity entity) {
