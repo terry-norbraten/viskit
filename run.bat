71f0fd4704751cf1ff8880d01be53a62812cebb0
rickg <rickg@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2005-10-12 19:44:36 +0000

    bug 249, 179
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@524 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- run.bat
+++ run.bat
@@ -1,5 +1,1 @@
-@REM Viskit run.bat
-@REM Add comments here
-java -version
-@REM -verbose 
-java -classpath "lib\jhall.jar;lib\actions.jar;lib\bsh-2.0b1.jar;lib\jdom.jar;lib\jgraph.jar;lib\simkit.jar;lib\dis.jar;lib\diskit.jar;lib\tools.jar;lib\trang.jar;lib\trangtask.jar;lib\jwsdp-1.3\dom.jar;lib\jwsdp-1.3\jax-qname.jar;lib\jwsdp-1.3\jaxb-api.jar;lib\jwsdp-1.3\jaxb-impl.jar;lib\jwsdp-1.3\jaxb-libs.jar;lib\jwsdp-1.3\jaxb-xjc.jar;lib\jwsdp-1.3\jaxp-api.jar;lib\jwsdp-1.3\namespace.jar;lib\jwsdp-1.3\relaxngDatatype.jar;lib\jwsdp-1.3\sax.jar;lib\jwsdp-1.3\xalan.jar;lib\jwsdp-1.3\xercesImpl.jar;lib\jwsdp-1.3\xsdlib.jar;lib\jwsdp-1.3\xsltc.jar;build\classes" viskit.Splash
+ant build-run-default
