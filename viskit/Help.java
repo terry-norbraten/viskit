ba00be0ef73d73a4391afaddf0ad4fd4547932de
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2005-10-20 15:15:29 +0000

    Updated version to 0.2.0 due to substantial changes in GUI and Assembly generation
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@537 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/Help.java
+++ viskit/Help.java
@@ -14,7 +14,7 @@ import javax.help.HelpSetException;
  */
 public class Help {
     
-    public static final String VERSION = "0.1.8";
+    public static final String VERSION = "0.2.0";
     
     private Component parent;
     private Icon icon;
