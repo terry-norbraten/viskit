18fcc90eaccd168c1064670e2100c180f380e3ea
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2005-11-10 00:29:52 +0000

    ticked to 0.2.6
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@630 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/Help.java
+++ viskit/Help.java
@@ -14,7 +14,7 @@ import javax.help.HelpSetException;
  */
 public class Help {
     
-    public static final String VERSION = "0.2.5";
+    public static final String VERSION = "0.2.6";
     
     private Component parent;
     private Icon icon;
