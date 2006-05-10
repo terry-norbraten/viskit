935b0a868efcaf8cc5da3dfab9740a3f0416ac8b
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2006-05-10 20:32:02 +0000

    Clicked version to 0.3.0
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@1011 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/Help.java
+++ viskit/Help.java
@@ -14,7 +14,7 @@ import javax.help.HelpSetException;
  */
 public class Help {
     
-    public static final String VERSION = "0.2.8";
+    public static final String VERSION = "0.3.0";
     
     private Component parent;
     private Icon icon;
