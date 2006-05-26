313329bfe63456d1742c2d1dcc73b0eaff762b36
rickg <rickg@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2006-05-26 22:52:40 +0000

    debug didn't know about Vstatics as that doesn't get compiled until after translators
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@1038 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/xsd/assembly/ViskitAssembly.java
+++ viskit/xsd/assembly/ViskitAssembly.java
@@ -37,7 +37,7 @@ public class ViskitAssembly extends BasicAssembly {
     protected LinkedHashMap replicationStatsListenerConnections;
     protected LinkedHashMap simEventListenerConnections;
     protected LinkedHashMap adapters;
-    private static boolean debug = viskit.Vstatics.debug;
+    private static boolean debug = false;
     
     /** Creates a new instance of ViskitAssembly */
     public ViskitAssembly() {
