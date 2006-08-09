a5b98d81e43b867f531d950f3c5236561a443605
ahbuss <ahbuss@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2006-08-09 23:55:11 +0000

    Bumped to 0.4.0 with new organization of panels
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@1175 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- viskit/Help.java
+++ viskit/Help.java
@@ -14,7 +14,7 @@ import javax.help.HelpSetException;
  */
 public class Help {
     
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.4.0";
     
     private Component parent;
     private Icon icon;
