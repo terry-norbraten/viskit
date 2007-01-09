ad7b6c44770b77a1cc64fe02a7311abd7c5298a5
jmbailey <jmbailey@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2007-01-09 17:41:59 +0000

    Put in correct classpath and set default directory to make script work
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@1288 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# This doesn't work 15 Mar 2005, needs classpath adjustment.  Should generate from build.xml.
-java -classpath build/classes:lib/simkit.jar:lib/bsh-2.0b1.jar:lib/jgraph.jar:lib/actions.jar:lib/jhall.jar:lib/tools.jar:lib/jdom.jar:lib/commons-lang-2.0.jar:lib/commons-configuration-1.1RC1.jar:lib/commons-collections-3.1.jar:lib/commons-beanutils-1.7.0.jar:lib/commons-logging-1.0.4.jar:lib/commons-digester-1.6.jar:lib/log4j-1.2.8.jar:lib/xerces.jar:lib/jwsdp-1.5/jaxb-impl.jar:lib/jwsdp-1.5/jaxb-libs.jar:lib/jwsdp-1.5/jaxb-xjc.jar:lib/jwsdp-1.5/xercesImpl.jar:lib/jwsdp-1.5/xsdlib.jar:lib/jwsdp-1.5/jaxb-api.jar:lib/jwsdp-1.5/jax-qname.jar:lib/jwsdp-1.5/relaxngDatatype.jar:lib/jwsdp-1.5/namespace.jar:lib/ext/dis.jar:lib/ext/diskit.jar viskit.Splash
+# todo: Generate from build.xml.
+SCRIPTDIR=`dirname $0`
+cd $SCRIPTDIR
+java -verbose -classpath ./lib/tools.jar:./build/classes:./lib/simkit.jar:./lib/bsh-2.0b4.jar:./lib/jgraph.jar:./lib/actions.jar:./lib/jhall.jar:./lib/jdom.jar:./lib/jfreechart-1.0.1.jar:./lib/jcommon-1.0.0.jar:./lib/junit.jar:./lib/commons-beanutils-1.7.0.jar:./lib/commons-codec-1.3.jar:./lib/commons-collections-3.1.jar:./lib/commons-configuration-1.1.jar:./lib/commons-digester-1.6.jar:./lib/commons-lang-2.0.jar:./lib/commons-logging-1.0.4.jar:./lib/log4j-1.2.8.jar:./lib/looks-1.3.1.jar:./lib/xerces.jar:./lib/xmlrpc-2.0.jar:./lib/jwsdp-1.5/jaxb-impl.jar:./lib/jwsdp-1.5/jaxb-libs.jar:./lib/jwsdp-1.5/jaxb-xjc.jar:./lib/jwsdp-1.5/xercesImpl.jar:./lib/jwsdp-1.5/xsdlib.jar:./lib/jwsdp-1.5/jaxb-api.jar:./lib/jwsdp-1.5/jax-qname.jar:./lib/jwsdp-1.5/relaxngDatatype.jar:./lib/jwsdp-1.5/namespace.jar:./lib/ext/dis.jar:./lib/ext/diskit.jar viskit.EventGraphAssemblyComboMain
+
