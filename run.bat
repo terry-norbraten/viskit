b59ec693e66e06d24a0b8debedd1e688ff055927
tdnorbra <tdnorbra@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2008-03-10 17:54:59 +0000

    update target call
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@1793 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-@REM Builds, then launches Viskit with module dependencies, i.e. Diskit
-ant build-run-default-extended
+@REM $Id:$
+@REM Builds, then launches Viskit with no runtime extensions, i.e. Diskit
+ant build.run.no.runtime.extensions
