e59aa8753b697259bdb88e87e243b60820d13720
tdnorbra <tdnorbra@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2009-03-10 11:44:55 +0000

    ensure new model when switching projects to clear old classpaths
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@2117 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- src/viskit/SettingsDialog.java
+++ src/viskit/SettingsDialog.java
@@ -159,7 +159,7 @@ public class SettingsDialog extends JDialog {
     private void buildWidgets() {
         JPanel classpathP = new JPanel();
         classpathP.setLayout(new BoxLayout(classpathP, BoxLayout.Y_AXIS));
-        classPathJlist = new JList(new DefaultListModel());
+        classPathJlist = new JList();
         JScrollPane jsp = new JScrollPane(classPathJlist);
         jsp.setPreferredSize(new Dimension(70, 70));  // don't want it to control size of dialog
         classpathP.add(jsp);
@@ -411,7 +411,7 @@ public class SettingsDialog extends JDialog {
     }
 
     private void fillWidgets() {
-        DefaultListModel mod = (DefaultListModel) classPathJlist.getModel();
+        DefaultListModel mod = new DefaultListModel();
         if (getExtraClassPath() != null) {
             String[] sa = getExtraClassPath();
             for (String s : sa) {
