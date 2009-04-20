57a7419064df3c89216b6f1ce69b61c666ef6883
tdnorbra <tdnorbra@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2009-04-20 23:02:49 +0000

    javadoc
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@2164 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- src/viskit/ViskitProject.java
+++ src/viskit/ViskitProject.java
@@ -383,6 +383,7 @@ public class ViskitProject {
         setProjectOpen(false);
     }
 
+    /** @return the root directory of this ViskitProject */
     public File getProjectRoot() {
         return projectRoot;
     }
