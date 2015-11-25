0e4eb207049b7977d7a9e38ca3d8158232e75105
brutzman <brutzman@0de750ab-c0a9-dc11-9d74-0011435b89d6>
2015-11-25 22:46:56 +0000

    add license, clear warnings
    git-svn-id: svn+ssh://eos.nps.edu/usr/local/svn/wcm/Viskit/trunk@2891 0de750ab-c0a9-dc11-9d74-0011435b89d6

--- src/viskit/view/AssemblyViewFrame.java
+++ src/viskit/view/AssemblyViewFrame.java
@@ -1,3 +1,37 @@
+/*
+Copyright (c) 1995-2015 held by the author(s).  All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer
+      in the documentation and/or other materials provided with the
+      distribution.
+    * Neither the names of the Naval Postgraduate School (NPS)
+      Modeling Virtual Environments and Simulation (MOVES) Institute
+      (http://www.nps.edu and http://www.movesinstitute.org)
+      nor the names of its contributors may be used to endorse or
+      promote products derived from this software without specific
+      prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package viskit.view;
 
 import viskit.jgraph.VgraphAssemblyComponentWrapper;
@@ -76,9 +110,9 @@ public class AssemblyViewFrame extends mvcAbstractJFrameView implements Assembly
 
     private final static String FRAME_DEFAULT_TITLE = " Viskit Assembly Editor";
 
-    private String FULLPATH = VStatics.FULL_PATH;
-    private String CLEARPATHFLAG = VStatics.CLEAR_PATH_FLAG;
-    private Color background = new Color(0xFB, 0xFB, 0xE5);
+    private final String FULLPATH = VStatics.FULL_PATH;
+    private final String CLEARPATHFLAG = VStatics.CLEAR_PATH_FLAG;
+    private final Color background = new Color(0xFB, 0xFB, 0xE5);
 
     /** Toolbar for dropping icons, connecting, etc. */
     private JTabbedPane tabbedPane;
@@ -513,7 +547,7 @@ public class AssemblyViewFrame extends mvcAbstractJFrameView implements Assembly
 
         class PortsVisibleListener implements ActionListener {
 
-            private boolean tOrF;
+            private final boolean tOrF;
 
             PortsVisibleListener(boolean tOrF) {
                 this.tOrF = tOrF;
@@ -794,9 +828,7 @@ public class AssemblyViewFrame extends mvcAbstractJFrameView implements Assembly
                     }
 
                     dragged = null;
-                } catch (UnsupportedFlavorException e) {
-                    LogUtils.getLogger(AssemblyViewFrame.class).error(e);
-                } catch (IOException e) {
+                } catch (UnsupportedFlavorException | IOException e) {
                     LogUtils.getLogger(AssemblyViewFrame.class).error(e);
                 }
             }
