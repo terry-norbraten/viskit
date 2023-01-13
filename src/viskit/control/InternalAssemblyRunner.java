f588fd9961d55699114b4149cc51f86cc6211cf9
git <tdnorbra@nps.edu>
2023-01-12 18:29:42 -0800

    [Terry N.] corr

--- src/viskit/control/InternalAssemblyRunner.java
+++ src/viskit/control/InternalAssemblyRunner.java
@@ -303,7 +303,7 @@ public class InternalAssemblyRunner implements PropertyChangeListener {
             new SimThreadMonitor(simRunner).start();
 
             // Restore Viskit's working ClassLoader
-//            Thread.currentThread().setContextClassLoader(lastLoaderNoReset);
+            Thread.currentThread().setContextClassLoader(lastLoaderNoReset);
 
         } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | InstantiationException | ClassNotFoundException ex) {
             LOG.error(ex);
@@ -400,7 +400,7 @@ public class InternalAssemblyRunner implements PropertyChangeListener {
 
                 if (assemblyInstance != null) {
 
-//                    Thread.currentThread().setContextClassLoader(lastLoaderWithReset);
+                    Thread.currentThread().setContextClassLoader(lastLoaderWithReset);
 
                     Method setStopRun = assemblyClass.getMethod("setStopRun", boolean.class);
                     setStopRun.invoke(assemblyInstance, true);
