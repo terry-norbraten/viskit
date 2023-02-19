cb56ce69e960137f89515bf25b9d091b6360f46f
git <tdnorbra@nps.edu>
2023-02-18 19:10:10 -0800

    use viskit.jar lastModified file property

--- src/viskit/util/Version.java
+++ src/viskit/util/Version.java
@@ -9,16 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.Properties;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.apache.logging.log4j.Logger;
 import viskit.VGlobals;
 
@@ -29,7 +25,6 @@ import viskit.VGlobals;
  * $Date: 2007-12-16 11:44:04 -0800 (Sun, 16 Dec 2007) $
  * </pre>
  *
- * @version $Id$
  * @author ahbuss
  */
 public class Version {
@@ -45,69 +40,68 @@ public class Version {
     protected int patchVersion;
     
     protected LocalDate lastModified;
-
-    protected int gitShaNumber;
+    
+    private Properties versionProperties;
 
     public Version(String fileName) {
+        
+        InputStream stream = null;
+        
+        versionProperties = new Properties();
+        
+        // version.txt is in the same package as VGlobals
         URL versionURL = VGlobals.class.getResource(fileName);
+        
+        if (versionURL.getProtocol().equals("jar"))
+            stream = VGlobals.class.getResourceAsStream(fileName);
+        else {
+            try {
+                stream = versionURL.openStream();
+            } catch (IOException ex) {
+                LOG.error("Problem reading {} due to {}: ", fileName, ex);
+            }
+        }
         try {
-            InputStream versionStream = versionURL.openStream();
-            Properties versionProperties = new Properties();
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(versionStream));
-            versionProperties.load(versionStream);
-            Path versionPath = Paths.get(versionURL.toURI());
+            if (stream != null)
+                versionProperties.load(stream);
+            else
+                LOG.error("Problem reading {} due to {}: ", fileName, null);
+        } catch (IOException ex) {
+            LOG.error(ex);
+        }
+        
+        Version.this.parseInputFile(versionURL);
+        
+        majorVersion = Version.this.parseVersionString(versionProperties.getProperty("major"));
+        minorVersion = Version.this.parseVersionString(versionProperties.getProperty("minor"));
+        patchVersion = Version.this.parseVersionString(versionProperties.getProperty("patch"));
+        versionString = String.format("%s.%s.%s", majorVersion, minorVersion, patchVersion);
+    }
+
+    protected int parseVersionString(String input) {
+        return Integer.parseInt(input);
+    }
+
+    protected void parseInputFile(URL versionURL) {
+        
+        Path versionPath = null;
+        
+        try {
+            
+            // Just use the viskit.jar lastModified file property
+            if (versionURL.getProtocol().equals("jar"))
+                versionPath = Paths.get(versionURL.getFile().substring(versionURL.getFile().indexOf(":") + 1, versionURL.getFile().indexOf("!")));
+            else
+                versionPath = Paths.get(versionURL.toURI());
             BasicFileAttributes attributes
                     = Files.readAttributes(versionPath, BasicFileAttributes.class);
             Date date = new Date(attributes.lastModifiedTime().toMillis());
             Instant instant = date.toInstant();
             ZonedDateTime zoneDateTime = instant.atZone(ZoneId.systemDefault());
             lastModified = zoneDateTime.toLocalDate();
-            majorVersion = Integer.parseInt(versionProperties.getProperty("major"));
-            minorVersion = Integer.parseInt(versionProperties.getProperty("minor"));
-            patchVersion = Integer.parseInt(versionProperties.getProperty("patch"));
-            versionString = String.format("%s.%s.%s", majorVersion, minorVersion, patchVersion);
-//            versionString = reader.readLine();
-//            int[] version = parseVersionString(versionString);
-//            majorVersion = version[0];
-//            minorVersion = version[1];
-//            patchVersion = version[2];
-//            String dateString = reader.readLine();
-//            lastModified = parseDateString(dateString);
-//            String revisionString = reader.readLine();
-//            gitShaNumber = parseRevisionString(revisionString);
-//            versionString += "." + gitShaNumber;
-        } catch (IOException | URISyntaxException e) {
-            LOG.error("Problem reading " + fileName + ": " + e);
+        } catch (IOException | URISyntaxException ex) {
+            LOG.error("Problem reading {} due to {}: ", versionPath, ex);
         }
-    }
-
-    protected static int[] parseVersionString(String versionString) {
-        String[] versions = versionString.split("\\.");
-        int[] versionNumber = new int[versions.length];
-        for (int i = 0; i < versionNumber.length; ++i) {
-            versionNumber[i] = Integer.parseInt(versions[i]);
-        }
-        return versionNumber;
-    }
-
-    protected static Date parseDateString(String dateString) {
-        Date date = null;
-        try {
-            Pattern pattern
-                    = Pattern.compile("\\d\\d\\d\\d\\-\\d\\d\\-\\d\\d \\d\\d:\\d\\d:\\d\\d");
-        Matcher matcher = pattern.matcher(dateString);
-        if (matcher.find()) {
-                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(matcher.group());
-        }
-        } catch (ParseException t) {
-            LOG.error("Problem parsing date string " + dateString + ": " + t);
-        }
-        return date;
-    }
-
-    protected static int parseRevisionString(String revisionString) {
-        String[] data = revisionString.split("\\D+");
-        return Integer.parseInt(data[1]);
     }
 
     public String getVersionString() {
@@ -128,21 +122,6 @@ public class Version {
 
     public int getPatchVersion() {
         return patchVersion;
-    }
-
-    public int getShaNumber() {
-        return gitShaNumber;
-    }
-
-    public boolean isSameVersionAs(String otherVersionString) {
-        return getVersionString().equals(otherVersionString);
-    }
-
-    public boolean isHigherVersionThan(String otherVersionString) {
-        int[] otherVersion = parseVersionString(otherVersionString);
-        return getMajorVersion() > otherVersion[0]
-                || getMinorVersion() > otherVersion[1]
-                || getPatchVersion() > otherVersion[2];
     }
 
     @Override
